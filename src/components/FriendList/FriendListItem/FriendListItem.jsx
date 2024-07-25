d04381a624c91897f5d3383dd6cd9cb8a0cc34ea
Lyuzika <lyuzika@gmail.com>
2024-07-25 16:29:04 +0300

    clsx add

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,6 +1,5 @@
 import css from "../FriendList.module.css"
 import { RiRadioButtonLine } from "react-icons/ri"
-
 import { IoCloudOffline } from "react-icons/io5"
 import clsx from "clsx"
 
@@ -10,7 +9,7 @@ export default function FriendListItem( {friend: { avatar, name, isOnline }}) {
     <div className={css.listItem}>
       <img className={css.ava} src={avatar} alt="Avatar" width="48" />
       <p className={css.name}>{name}</p>
-      <p className={isOnline ? css.online : css.offline}>
+      <p className={clsx(isOnline ? css.online : css.offline)}>
         {isOnline ? <RiRadioButtonLine /> : <IoCloudOffline /> }</p>
     </div>
   )
